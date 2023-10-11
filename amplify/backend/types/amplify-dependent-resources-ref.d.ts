94f8598125a0317256c9ca1ae3e504cece8ae9a8
Clement-Wright <clewri163@gmail.com>
2023-10-10 20:35:51 -0400

    Q

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "AmplifyApp2Bucket": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
